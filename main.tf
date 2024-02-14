1082c05b745bb88834b6aed28b7ee591ce31d74a
OMER ABDULLAH <98330268+omer-abdulla@users.noreply.github.com>
2024-02-14 17:21:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,31 @@
-# main.tf
+# Terraform
 
-# Create SNS topic
+# SNS Topic
 resource "aws_sns_topic" "my_topic" {
   name = "my-topic"
 }
 
-# Create Lambda function subscribed to SNS topic
+# Lambda Function
 resource "aws_lambda_function" "my_function" {
-  function_name = "my-function"
+  function_name    = "my-function"
+  runtime          = "python3.7"
+  handler          = "index.handler"
 
-  s3_bucket = "my-bucket"
-  s3_key    = "my-function.zip"
+  s3_bucket        = "my-bucket"
+  s3_key           = "my-function.zip"
 
-  handler = "index.handler"
-  runtime = "python3.7"
-
-  source_arn = aws_sns_topic.my_topic.arn
+  source_arn       = aws_sns_topic.my_topic.arn
 }
 
-# Create DynamoDB table
+# DynamoDB Table
 resource "aws_dynamodb_table" "my_table" {
-  name           = "my-table"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "id"
+  name             = "my-table"
+  read_capacity    = 5
+  write_capacity   = 5
+  hash_key         = "id"
 
   attribute {
     name = "id"
-    type = "S"
+    type = "S" 
   }
 }
