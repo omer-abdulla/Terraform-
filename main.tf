e239171083a5570caa0f680af8149a2e850ab00f
OMER ABDULLAH <98330268+omer-abdulla@users.noreply.github.com>
2024-02-14 17:07:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
-provider "aws" {
-  region = "us-east-1"  # Update with your desired AWS region
-}
+provider:
+  aws:
+    region: us-east-1
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
-  
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
+resources:
+  - type: aws_iam_role
+    name: lambda_role
+    config:
+      name: lambda_execution_role
+      assume_role_policy: |
+        {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Principal": {
+                "Service": "lambda.amazonaws.com"
+              },
+              "Action": "sts:AssumeRole"
+            }
+          ]
         }
-        Action    = "sts:AssumeRole"
-      },
-    ]
-  })
-  
-  # Define additional IAM policies as needed
-}
 
-resource "aws_lambda_function" "example_lambda" {
-  function_name = "example_lambda_function"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  role          = aws_iam_role.lambda_role.arn
-
-  # Define the Lambda function code here
-  # e.g., source_code_hash = filebase64("path/to/lambda_function.zip")
-}
+  - type: aws_lambda_function
+    name: example_lambda
+    config:
+      function_name: example_lambda_function
+      handler: index.handler
+      runtime: nodejs14.x
+      role: ${aws_iam_role.lambda_role.arn}
+      # Define the Lambda function code here
+      # e.g., source_code_hash: ${filebase64("path/to/lambda_function.zip")}
