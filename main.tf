13e112a30044bbd04fd4c05ba6ddb8f54ec045dc
OMER ABDULLAH <98330268+omer-abdulla@users.noreply.github.com>
2024-02-14 13:10:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,32 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Update with your desired AWS region
 }
 
-resource "aws_sns_topic" "example" {
-  name = "example-topic" 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
+      },
+    ]
+  })
+  
+  # Define additional IAM policies as needed
 }
 
-resource "aws_lambda_function" "example" {
-  function_name = "example-lambda"
+resource "aws_lambda_function" "example_lambda" {
+  function_name = "example_lambda_function"
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.lambda_role.arn
 
-  # runtime, role etc
-
-  triggers = {
-    example-trigger = aws_sns_topic.example.arn
-  }
-}
-
-resource "aws_dynamodb_table" "example" {
-  name           = "example-table"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "Id"
-
-  attribute {
-    name = "Id" 
-    type = "S"
-  }
+  # Define the Lambda function code here
+  # e.g., source_code_hash = filebase64("path/to/lambda_function.zip")
 }
